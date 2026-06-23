cd67b10d700dcfbe97c3a58a1155f7897ddf7c17
xuqigui <xuqigui@agibot.com>
2026-06-23 21:40:57 +0800

    [win] update

--- windows/x64/cpp/demo/omni_picker_2025/OP1_demo_canfd_id.cc
+++ windows/x64/cpp/demo/omni_picker_2025/OP1_demo_canfd_id.cc
@@ -1,18 +1,40 @@
 #include <chrono>
+#include <cstdint>
 #include <cstdlib>
+#include <exception>
+#include <filesystem>
 #include <iostream>
 #include <memory>
+#include <stdexcept>
 #include <string>
 #include <thread>
 #include <vector>
 #include "omnihand/omni_picker_2025.h"
 #include "omnihand/proto.h"
 static std::unique_ptr<agilink::omnihand::OmniPicker2025> picker;
-
+enum class Commands : uint8_t {
+  PATH = 1,
+  NONE = 0xff
+};
 int main(int argc, char** argv) {
   std::vector<std::string> args;
-  for (int i = 0; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     args.push_back(argv[i]);
+  }
+  std::string input_path = "";
+  auto prev = Commands::NONE;
+  for (auto& arg : args) {
+    if (prev != Commands::NONE) {
+      if (prev == Commands::PATH) {
+        input_path = arg;
+      }
+      continue;
+    }
+    if (arg == "--path" || arg == "-p") {
+      prev = Commands::PATH;
+      continue;
+    }
+    throw std::runtime_error("error occupid when process " + arg);
   }
   picker = agilink::omnihand::OmniPicker2025::createHandByHcan(
       agilink::omnihand::HandType::LEFT,
@@ -23,8 +45,8 @@ int main(int argc, char** argv) {
     std::cout << "[ERROR][INIT] error init omnipicker 2025" << std::endl;
     return -1;
   }
-  std::cout << picker->GetDeviceInfo().ToString() << std::endl;
-
+  // std::cout << picker->GetDeviceInfo().ToString() << std::endl;
+  picker->ShowDataDetails(true);
   agilink::omnihand::Op1ControlFrame frame{};
   frame.pos_cmd = 0x00;
   frame.vel_cmd = 0xFF;
@@ -35,6 +57,19 @@ int main(int argc, char** argv) {
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   frame.pos_cmd = 0xFF;
   picker->SendFrameSync(frame);
-
+  if (input_path == "")
+    return 0;
+  std::filesystem::path otaFilePath(input_path);
+  if (!std::filesystem::exists(otaFilePath)) {
+    throw std::runtime_error("input file is not existed");
+  }
+  picker->UpdateFirmware(otaFilePath.string());
+  std::cout << "[INFO][UPDATE_FIRMWARE] update firmware success" << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  frame.pos_cmd = 0x00;
+  picker->SendFrameSync(frame);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  frame.pos_cmd = 0xFF;
+  picker->SendFrameSync(frame);
   return 0;
 }
