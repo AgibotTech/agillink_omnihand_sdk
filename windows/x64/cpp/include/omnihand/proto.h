abb72bdcfeb875aaf3146f3c787051cde657aa51
xuqigui <xuqigui@agibot.com>
2026-08-28 12:50:16 +0800

    [win] fix device info, fix cmakelist for xense

--- windows/x64/cpp/include/omnihand/proto.h
+++ windows/x64/cpp/include/omnihand/proto.h
@@ -269,10 +269,8 @@ struct AGIBOT_EXPORT Version {
     std::stringstream sstream;
     sstream << static_cast<unsigned int>(major) << "."
             << static_cast<unsigned int>(minor) << "."
-            << static_cast<unsigned int>(patch);
-    if (res != 0) {
-      sstream << "." << static_cast<unsigned int>(res);
-    }
+            << static_cast<unsigned int>(patch) << "."
+            << static_cast<unsigned int>(res);
     return sstream.str();
   }
 };
@@ -332,7 +330,7 @@ struct AGIBOT_EXPORT CommuParams {
 struct AGIBOT_EXPORT DeviceInfo {
   uint8_t hand_device_id;
   CommuParams commu_params;
-  HandType hand_type;
+  HandType hand_type{HandType::UNKNOWN};
 
   std::string ToString() const {
     std::stringstream sstream;
